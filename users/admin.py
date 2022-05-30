a22a6f7458cd00379669e0b7eac9e94c239ed009
daydream-03 <byjung1230@gmail.com>
2022-05-30 21:36:13 +0900

    add rooms models

--- users/admin.py
+++ users/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth.admin import UserAdmin
 from . import models
 
 
-@admin.register(models.User)
+@admin.register(models.User)  # == admin.site.register(models.User, CustomUserAdmin)
 class CustomUserAdmin(UserAdmin):
 
     """Custom User Admin"""
